a4b2b9aeeb45f4f6581a8f2cdd0e2c2a09199e53
brianseltzer1738 <brianseltzer@hanovernorwichschools.org>
2017-05-10 13:39:09 -0400

    Updated from Brython Server: 5/10/2017 1:39:09 PM

--- pong.py
+++ pong.py
@@ -1,1 +1,26 @@
 
+import pygame, sys
+from pygame.locals import*
+FPS=200
+windowidth=400
+windowhight=300
+def main():
+    pygame.init()
+    global DISPLAYSURF
+    
+    FPSclock=pygame.time.clock()
+    DISPLAYSURF=pygame.display.set_mode((windowwith,windowhight))
+    pyhame.display.set_caption('Pong')
+    
+    while True: #main game loop
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+
+        pygame.display.update()
+        FPSclock.tick(FPS)
+
+if __name__=='__main__':
+    main()
+
